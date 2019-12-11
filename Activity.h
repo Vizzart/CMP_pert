abf4d3b58ece55d01198ffa2d674e977123b72f1
Maciej Rewera <22579925+MaciejRewera@users.noreply.github.com>
2019-12-11 08:54:27 +0000

    Fixed duration algorithm

--- Activity.h
+++ Activity.h
@@ -15,8 +15,10 @@ private:
     double duration = 0.0;
     std::vector<Activity*> *parents = new std::vector<Activity*>();
 
-    double left = 0.0;
-    double right = 0.0;
+    double longestParentsDuration = 0.0;
+    double parentsAndMyDuration = 0.0;
+
+    double latestStartTime = 0.0;
 
 public:
     std::string name = "";
@@ -27,10 +29,10 @@ public:
         parents = aParents;
     };
 
-    double getLongestParentsDuration() {
+    double calcTotalDuration() {
         if (parents->empty()) {
             double totalValue = this->duration;
-            this->left = totalValue;
+            this->parentsAndMyDuration = totalValue;
 
             return totalValue;
         }
@@ -38,15 +40,16 @@ public:
             std::vector<double> results = std::vector<double>(parents->size());
             // Tak wygląda for each w C++. Po prostu idziesz po wszystkich elementach w kolekcji
             for (auto &parent : *parents) {
-                results.push_back(parent->getLongestParentsDuration());
+                results.push_back(parent->calcTotalDuration());
             }
 
-            double maxValue = *std::max_element(results.begin(), results.end());
+            double longestDuration = *std::max_element(results.begin(), results.end());
 
-            double totalValue = maxValue + this->duration;
-            this->left = totalValue;
+            double totalDuration = longestDuration + this->duration;
+            this->longestParentsDuration = longestDuration;
+            this->parentsAndMyDuration = totalDuration;
 
-            return totalValue;
+            return totalDuration;
         }
     }
 
@@ -56,7 +59,7 @@ public:
             parentsNames += (" " + parent->name);
         }
 
-        return "\n\nname: " + name + "\nduration: " + std::to_string(duration) + "\nleft: " + std::to_string(left) + "\nright: " + std::to_string(right) + "\nparents: " + parentsNames;
+        return "\n\nname: " + name + "\nduration: " + std::to_string(duration) + "\nlongestParentsDuration: " + std::to_string(longestParentsDuration) + "\nparentsAndMyDuration: " + std::to_string(parentsAndMyDuration) + "\nparents: " + parentsNames;
     }
 };
 
